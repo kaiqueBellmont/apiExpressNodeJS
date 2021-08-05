93a593f6a69bcd7ae474d64371a75dc7a400f457
kaiquebellmont <kaiquebellmont@gmail.com.br>
2021-08-05 15:53:03 -0300

    adicionando readme

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,11 @@
-const {getUsers, getUsersById, putUserById, createUser} = require('../src/controllers/userController');
+const {getUsers, getUsersById, UpdateUser, createUser} = require('../src/controllers/userController');
 
 const router = require('express').Router();
 
 //User Routes
 router.get('/users', getUsers);
 router.get('/users/:id', getUsersById);
-router.put('/users/:id', putUserById);
+router.put('/users/:id', UpdateUser);
 router.post('/users', createUser);
 
 
